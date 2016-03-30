9bcb8e351d8765fb52e01690a0e01e2d384417d5
haaspt <patrick.tyler.haas@gmail.com>
2016-03-29 22:34:51 -0700

    Output now saves to /data/ dir

--- io_utils.py
+++ io_utils.py
@@ -1,16 +1,15 @@
-import re
 import pandas as pd
 from time import strftime
 
-def dumps_like_a_truck(dataframe, filename=None):
+def dumps_like_a_truck(dataframe, filename=None, append=False):
     """Saves collected dataframe to a serialized format
     Currently this uses the pandas experimental msgpack binary format
     
     This may be edited in the future if it proves problematic"""
 
-    filename = strftime("%m_%d_%y.%H.%M.%S") + ".msg"
+    filename = "./data/" + strftime("%m_%d_%y.%H.%M.%S") + ".msg"
     
-    dataframe.to_msgpack(filename)
+    dataframe.to_msgpack(filename, append=append)
 
 
 def pick_it_up(filename):
