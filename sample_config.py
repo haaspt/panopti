14d5485d2ddb7d63b6ccf5c1f18758cafee1de3e
haaspt <patrick.tyler.haas@gmail.com>
2016-03-30 20:05:02 -0700

    Fixed dumb syntax error

--- sample_config.py
+++ sample_config.py
@@ -6,5 +6,5 @@ class GlobalConfig(object):
     """Basic configuration"""
 
     def __init__(self):
-        self.network_hub: "all" # Replace with subreddit name (the part after /r/)
-        self.user_agent: "Social network mapper, monitored by /u/YourUserName"
+        self.network_hub =  "all" # Replace with subreddit name (the part after /r/)
+        self.user_agent = "Social network mapper, monitored by /u/YourUserName"
