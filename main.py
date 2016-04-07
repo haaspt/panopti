0d799b80f7f9880634bee2381c537a37ff320161
haaspt <patrick.tyler.haas@gmail.com>
2016-04-06 23:11:20 -0700

    Added: get_user_comments

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from config import GlobalConfig
 options = GlobalConfig()
 reddit = praw.Reddit(user_agent = options.user_agent)
 
-new_posts = reddit.get_subreddit(options.network_hub).get_new(limit=1000)
+new_posts = reddit.get_subreddit(options.network_hub).get_new(limit=10)
 
 def get_new_authors(post_list, author_list=None):
     """Basic syntax to gather a list of post and comment authors
@@ -23,9 +23,19 @@ def get_new_authors(post_list, author_list=None):
     if not author_list:
         author_list = []
 
+    counter = 0
+
     for post in post_list:
         author_list.append(post.author)
+        counter += 1
+        print("Authors added: %d" % counter, end="\r")
         for comment in post.comments:
             author_list.append(comment.author)
+            counter += 1
+            print("Authors added: %d" % counter, end="\r")
 
     return author_list
+
+def get_user_comments(username):
+    # I'm fairly positive this doesn't actually work
+    user_comments = user.get_submitted(limit=10)
