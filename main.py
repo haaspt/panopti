4ec7fb12e5db7ee27aed24225aca23ef9f8d5034
haaspt <patrick.tyler.haas@gmail.com>
2016-04-10 14:03:16 -0700

    Added: submission parsing

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import io_utils
 import scraper
 import praw
 import pandas as pd
+import numpy as np
 from config import Config
 
 options = Config()
@@ -33,6 +34,7 @@ def get_new_authors(reddit_post_generator, author_list=None):
     print('%d new authors found!' % (len(author_df.user_name)))
     return author_df
 
+
 def comment_parser(reddit_comment_object):
 
     post_timestamp = reddit_comment_object.created_utc
@@ -41,21 +43,36 @@ def comment_parser(reddit_comment_object):
     ups = reddit_comment_object.ups
     downs = reddit_comment_object.downs
     post_body = reddit_comment_object.body
-    link_title = reddit_comment_object.link_title
-    link_id = reddit_comment_object.link_id
-    link_author = reddit_comment_object.link_author
+    thread_title = reddit_comment_object.link_title
+    thread_url = reddit_comment_object.link_url
     subreddit = reddit_comment_object.subreddit.display_name
 
-    return post_timestamp, post_id, score, ups, downs, post_body, link_title, link_id, link_author, subreddit
+    return post_timestamp, post_id, score, ups, downs, post_body, thread_title, thread_url, subreddit
 
 
-def get_user_comments(reddit_user_object, comment_dataframe=None):
+def submission_parser(reddit_submission_object):
+
+    post_timestamp = reddit_submission_object.created_utc
+    post_id = reddit_submission_object.id
+    score = reddit_submission_object.score
+    ups = reddit_submission_object.ups
+    downs = reddit_submission_object.downs
+    # post_body = np.nan
+    thread_title = reddit_submission_object.title
+    thread_url = reddit_submission_object.url
+    subreddit = reddit_submission_object.subreddit.display_name
+
+    return post_timestamp, post_id, score, ups, downs, thread_title, thread_url, subreddit
+
+
+def get_user_comments(reddit_user_object, content_dataframe=None):
 
     user_name = reddit_user_object.name
     user_comments = reddit_user_object.get_comments(limit=1000) # Due to reddit's caching, 1000 is the absolute max
 
-    if not comment_dataframe:
-        comment_dataframe = pd.DataFrame(columns=[
+    if not content_dataframe:
+        content_dataframe = pd.DataFrame(columns=[
+        'object_type',
         'user_name',
         'post_timestamp',
         'post_id',
@@ -63,16 +80,16 @@ def get_user_comments(reddit_user_object, comment_dataframe=None):
         'ups',
         'downs',
         'post_body',
-        'link_title',
-        'link_id',
-        'link_author',
+        'thread_title',
+        'thread_url',
         'subreddit',
         'timestamp'
         ])
 
     for comment in user_comments:
-        post_timestamp, post_id, score, ups, downs, post_body, link_title, link_id, link_author, subreddit = comment_parser(comment)
-        comment_dataframe = comment_dataframe.append({
+        post_timestamp, post_id, score, ups, downs, post_body, thread_title, thread_url, subreddit = comment_parser(comment)
+        content_dataframe = content_dataframe.append({
+        'object_type': 'comment',
         'user_name': user_name,
         'post_timestamp': post_timestamp,
         'post_id': post_id,
@@ -80,10 +97,49 @@ def get_user_comments(reddit_user_object, comment_dataframe=None):
         'ups': ups,
         'downs': downs,
         'post_body': post_body,
-        'link_title': link_title,
-        'link_id': link_id,
-        'link_author': link_author,
+        'thread_title': thread_title,
+        'thread_url': thread_url,
         'subreddit': subreddit,
         'timestamp': time.time()}, ignore_index=True)
 
-    return comment_dataframe
+    return content_dataframe
+
+
+def get_user_submissions(reddit_user_object, content_dataframe=None):
+
+    user_name = reddit_user_object.name
+    user_submissions = reddit_user_object.get_submitted(limit=1000) # Due to reddit's caching, 1000 is the absolute max
+
+    if not content_dataframe:
+        content_dataframe = pd.DataFrame(columns=[
+        'object_type',
+        'user_name',
+        'post_timestamp',
+        'post_id',
+        'score',
+        'ups',
+        'downs',
+        'post_body',
+        'thread_title',
+        'thread_url',
+        'subreddit',
+        'timestamp'
+        ])
+
+    for submission in user_submissions:
+        post_timestamp, post_id, score, ups, downs, thread_title, thread_url, subreddit = submission_parser(submission)
+        content_dataframe = content_dataframe.append({
+        'object_type': 'submission',
+        'user_name': user_name,
+        'post_timestamp': post_timestamp,
+        'post_id': post_id,
+        'score': score,
+        'ups': ups,
+        'downs': downs,
+        'post_body': np.nan,
+        'thread_title': thread_title,
+        'thread_url': thread_url,
+        'subreddit': subreddit,
+        'timestamp': time.time()}, ignore_index=True)
+
+    return content_dataframe
