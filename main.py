6e4e5f83a350b145e03919698522bfd649e10b0c
haaspt <patrick.tyler.haas@gmail.com>
2016-04-10 12:34:24 -0700

    Added: get_new_authors now returns a pandas dataframe

--- main.py
+++ main.py
@@ -10,31 +10,27 @@ reddit = praw.Reddit(user_agent = options.user_agent)
 
 new_posts = reddit.get_subreddit(options.network_hub).get_new(limit=options.post_limit)
 
-def get_new_authors(post_list, author_list=None):
+def get_new_authors(post_generator, author_list=None):
     """Basic syntax to gather a list of post and comment authors
     gathered from new posts on the target subreddit
 
     To Do:
     - More structured data processing
-    - Display counter
     - Maybe flatten the comment tree?
     """
 
     if not author_list:
         author_list = []
 
-    counter = 0
-
-    for post in post_list:
-        author_list.append(post.author)
-        counter += 1
-        print("Authors added: %d" % counter, end="\r")
+    for post in post_generator:
+        author_list.append(post.author.name)
         for comment in post.comments:
-            author_list.append(comment.author)
-            counter += 1
-            print("Authors added: %d" % counter, end="\r")
+            author_list.append(comment.author.name)
 
-    return author_list
+    author_df = pd.DataFrame({'user_name': author_list, 'highest_post_id': ""})
+    author_df = author_df.drop_duplicates('user_name')
+    print('%d new authors found!' % (len(author_df.user_name)))
+    return author_df
 
 def get_user_comments(username):
     # I'm fairly positive this doesn't actually work
