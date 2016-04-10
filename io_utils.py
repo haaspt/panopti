3dd5b132d18bb93e878fabfdb206d9021e8136c2
haaspt <patrick.tyler.haas@gmail.com>
2016-04-10 14:52:28 -0700

    Changed: file format switched to pickle

--- io_utils.py
+++ io_utils.py
@@ -4,17 +4,17 @@ from time import strftime
 def dumps_like_a_truck(dataframe, filename=None, append=False):
     """Saves collected dataframe to a serialized format
     Currently this uses the pandas experimental msgpack binary format
-    
-    This may be edited in the future if it proves problematic"""
-    
-    if filename is None:
-        filename = "./data/" + strftime("%m_%d_%y.%H.%M.%S") + ".msg"
 
-    dataframe.to_msgpack(filename, append=append)
+    This may be edited in the future if it proves problematic"""
+
+    if filename is None:
+        filename = "./data/" + strftime("%m_%d_%y.%H.%M.%S") + ".pkl"
+
+    dataframe.to_pickle(filename, append=append)
 
 
 def pick_it_up(filename):
     """Loads a saved msgpack
     By default it creates a new dataframe named after the file"""
 
-    return pd.read_msgpack(filename)
+    return pd.read_pickle(filename)
