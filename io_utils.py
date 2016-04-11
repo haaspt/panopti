3a19833efbf0f8ffa3548f384978f81c4b141adc
haaspt <patrick.tyler.haas@gmail.com>
2016-04-10 19:22:50 -0700

    Tweak: documentation

--- io_utils.py
+++ io_utils.py
@@ -2,10 +2,7 @@ import pandas as pd
 from time import strftime
 
 def dumps_like_a_truck(dataframe, filename=None, append=False):
-    """Saves collected dataframe to a serialized format
-    Currently this uses the pandas experimental msgpack binary format
-
-    This may be edited in the future if it proves problematic"""
+    """Saves collected dataframe to a serialized pickle format"""
 
     if filename is None:
         filename = "./data/" + strftime("%m_%d_%y.%H.%M.%S") + ".pkl"
@@ -14,7 +11,7 @@ def dumps_like_a_truck(dataframe, filename=None, append=False):
 
 
 def pick_it_up(filename):
-    """Loads a saved msgpack
+    """Loads a saved pickle file
     By default it creates a new dataframe named after the file"""
 
     return pd.read_pickle(filename)
