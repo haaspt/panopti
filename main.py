5c8c509551861562821019a75c5601b362cc9149
haaspt <patrick.tyler.haas@gmail.com>
2016-04-11 18:47:27 -0700

    Removed: disabled author logging, currently broken

--- main.py
+++ main.py
@@ -11,20 +11,20 @@ def main():
     options = Config()
     reddit = praw.Reddit(user_agent = options.user_agent)
 
-    post_generator = reddit.get_subreddit(options.network_hub).get_top(limit=options.post_limit)
+    post_generator = reddit.get_subreddit(options.network_hub).get_new(limit=options.post_limit)
 
     user_series = scraper.get_new_authors(post_generator)
     content_df = pd.DataFrame()
-    log_df = io_utils.load_log()
+    #log_df = io_utils.load_log()
 
     for user in user_series:
         content_df = scraper.get_user_comments(user, content_dataframe=content_df)
         content_df = scraper.get_user_submissions(user, content_dataframe=content_df)
 
-        log_df = scraper.log_user(user, log_dataframe=log_df)
+        #log_df = scraper.log_user(user, log_dataframe=log_df)
 
     io_utils.dumps_like_a_truck(content_df)
-    io_utils.save_log(log_df)
+    #io_utils.save_log(log_df)
 
 
 if __name__ == "__main__":
